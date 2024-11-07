b6deb7e499a41912a51a1e460819371e54e05d99
Wahid Sadique Ayan <wsayan28@gmail.com>
2024-11-07 20:56:17 +0600

    fix merged errors

--- src/features/syllabus/syllabus_routes.py
+++ src/features/syllabus/syllabus_routes.py
@@ -3,7 +3,6 @@ from http.client import HTTPException
 from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends, status, File, UploadFile
 
-from src.features.auth.utils.oauth2_utils import oauth2_scheme
 from src.features.syllabus.services.syllabus_service_contract import SyllabusServiceContract
 from src.features.syllabus.syllabus_container import SyllabusContainer
 
@@ -13,11 +12,9 @@ router = APIRouter(prefix="/syllabus")
 @router.post("/save", status_code=status.HTTP_201_CREATED, summary="Save Syllabus")
 @inject
 async def save(
-        token: str = Depends(oauth2_scheme),
         file: UploadFile = File(...),
         syllabus_service: SyllabusServiceContract = Depends(Provide[SyllabusContainer.syllabus_service])
 ):
-    # Check if the uploaded file is an XML file
     if not file.filename.endswith(".xml"):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Only XML files are allowed.")
 
@@ -28,7 +25,6 @@ async def save(
 @router.get("/getByDeptID", summary="Get department wise syllabus")
 @inject
 async def getByDepartmentID(
-        token: str = Depends(oauth2_scheme),
         department_id: int = None,
         syllabus_service: SyllabusServiceContract = Depends(Provide[SyllabusContainer.syllabus_service])
 ):

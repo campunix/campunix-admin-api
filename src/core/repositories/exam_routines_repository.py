46ef6cb865f73250bd84afdde73e4cda6652d365
Wahid Sadique Ayan <wsayan28@gmail.com>
2025-02-16 22:41:03 +0600

    exam routine repos

---
+++ src/core/repositories/exam_routines_repository.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.contracts.exam_routines_repository_contract import ExamRoutinesRepositoryContract
+from src.core.entities.exam_routine.exam_routines import ExamRoutine
+from src.infrastructure.repositories.base_repository import BaseRepository
+
+
+class ExamRoutinesRepository(
+    BaseRepository[ExamRoutine], ExamRoutinesRepositoryContract
+):
+    def __init__(self, db_session: AsyncSession):
+        super().__init__(db_session, ExamRoutine)
